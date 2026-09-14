d302da1902f14697c3e56dd98831f7661e5d16f4
binhxcz <superping36@gmail.com>
2026-09-14 20:00:38 +0700

    update create table in database

---
+++ src/core/db/database.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+def init_db():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS road_map(
+    id INTEGER PRIMARY KEY,
+    goal_title VARCHAR(255) NOT NULL,
+    summary VARCHAR(255) NOT NULL,
+    total_estimated_days INTEGER NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS phase_overview(
+        id INTEGER PRIMARY KEY,
+        phase_number INTEGER NOT NULL,
+        phase_title VARCHAR(255) NOT NULL,
+        phase_description VARCHAR(255),
+        duration_days INTEGER NOT NULL,
+        key_outcome VARCHAR(255)
+        road_map_id INTEGER REFERENCES road_map(id)
+        );
+
+    CREATE TABLE IF NOT EXISTS phase_task(
+        id INTEGER PRIMARY KEY,
+        phase_number INTEGER NOT NULL,
+        week_number INTEGER NOT NULL,
+        week_start_date DATE
+        );
+
+    CREATE TABLE IF NOT EXISTS task(
+        id INTEGER PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        notes VARCHAR(255),
+        calendar_start TIMESTAMPTZ NOT NULL,
+        calendar_end TIMESTAMPTZ NOT NULL
+        );
+"""
+    with engine.begin() as conn:
+        conn.execute(text(create_table_query))
